369b068f815e3492d25d148955964f0908417e37
KauanRdzin <89669838+KauanRdzin@users.noreply.github.com>
2021-08-27 22:58:01 -0300

    Create questao17,1.java

---
+++ questao17,1.java
@@ -0,0 +1,9 @@
+# lista-de-exercicios-2-KauanRdzin
+lista-de-exercicios-2-KauanRdzin created by GitHub Classroom
+public class Questao17Teste {
+    public static void main(String[] args) {
+        Questao17 num = new Questao17();
+        num.setNum(345);
+        num.mosRom();
+    }
+}
