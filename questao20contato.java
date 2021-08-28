7ae91c3bed4a32441edd6eb6abdbfc8ff51f5c7b
KauanRdzin <89669838+KauanRdzin@users.noreply.github.com>
2021-08-27 23:02:40 -0300

    Create questao20contato.java

---
+++ questao20contato.java
@@ -0,0 +1,28 @@
+# lista-de-exercicios-2-KauanRdzin
+lista-de-exercicios-2-KauanRdzin created by GitHub Classroom
+public class questao20contato {
+    private static String nome;
+    private static int cod;
+    private static String num;
+
+    public void setNome(String n){
+        nome = n;
+    }
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNum(String n){
+        num = n;
+    }
+    public String getNum(){
+        return num;
+    }
+
+    public void setCod(int c){
+        cod = c;
+    }
+    public int getCod(){
+        return cod;
+    }
+}
