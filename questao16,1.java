50bca6ee7b3b9e16c9d7c043ee9a763ca8f09a67
KauanRdzin <89669838+KauanRdzin@users.noreply.github.com>
2021-08-27 22:56:01 -0300

    Create questao16,1.java

---
+++ questao16,1.java
@@ -0,0 +1,9 @@
+# lista-de-exercicios-2-KauanRdzin
+lista-de-exercicios-2-KauanRdzin created by GitHub Classroom
+public class questao16_1 {
+    public static void main(String[] args) {
+        Questao16 j = new Questao16();
+        j.setTempo(48679);
+        j.status();
+    }
+}
