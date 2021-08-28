9f59a5889536625f35ba807a6dc279783b77a6d0
KauanRdzin <89669838+KauanRdzin@users.noreply.github.com>
2021-08-27 23:09:52 -0300

    Update questao20obj.java

--- questao20obj.java
+++ questao20obj.java
@@ -2,6 +2,6 @@
 lista-de-exercicios-2-KauanRdzin created by GitHub Classroom
 public class Questao20Teste {
     public static void main(String[] args) {
-        Questao20Agenda c1 = new Questao20Agenda();
+        Questao20Agenda c1 = new questao20agenda();
         c1.setContato("Kauan Rodrigo Souza Damasceno",);
         c1.status();
